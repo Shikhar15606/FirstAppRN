db848177f782ae88643d69f903cf8ec1b6920dea
Shikhar15606 <shikharshukla15606@gmail.com>
2021-02-12 00:05:04 +0530

    Flex Intro

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View ,TextInput } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   const [message, setmessage] = useState('My lovely app')
 
   return (
     <View>
-      <View style={{padding:50}}>
-        <TextInput placeholder="Enter your Goal" style={{borderBottomColor:"black", borderBottomWidth:1, padding:10}}></TextInput>
+      <View style={{ padding: 50, flexDirection: 'row', justifyContent: "space-between", alignItems: 'center' }}>
+        {/* alignItems for cross axis */}
+        {/* justify content for main axis */}
+        <TextInput placeholder="Enter your Goal" style={{ borderBottomColor: "black", borderBottomWidth: 1, padding: 10, width: '80%' }}></TextInput>
         <Button title="ADD"></Button>
       </View>
       <View>
