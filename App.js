584c03c0a781c34818c5c8fae037714442736d6f
Shikhar15606 <shikharshukla15606@gmail.com>
2021-02-13 20:08:43 +0530

    Modal for Input

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import InputGoals from './Components/InputGoals';
 import ListGoals from './Components/ListGoals';
 
 export default function App() {
-
+  const [isModalOpen, setisModalOpen] = useState(false);
   const [listofGoals, setlistofGoals] = useState([]);
 
   const addGoalToList = (currentGoal) => {
@@ -13,6 +13,14 @@ export default function App() {
 
   const removeGoalfromList = (goalid) => {
     setlistofGoals(listofGoals => listofGoals.filter(goal => goal.mykey !== goalid))
+  }
+
+  const openModal = () => {
+    setisModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setisModalOpen(false);
   }
 
   const styles = StyleSheet.create({
@@ -24,7 +32,8 @@ export default function App() {
 
   return (
     <View style={styles.root}>
-      <InputGoals addGoalToList={addGoalToList} />
+      <Button title="Add New Goals" onPress={openModal} />
+      <InputGoals addGoalToList={addGoalToList} isModalOpen={isModalOpen} closeModal={closeModal} />
       <ListGoals listofGoals={listofGoals} removeGoalfromList={removeGoalfromList} />
     </View>
   );
--- Components/InputGoals.js
+++ Components/InputGoals.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, View, TextInput } from 'react-native';
+import { Button, StyleSheet, View, TextInput, Modal } from 'react-native';
 
-const InputGoals = ({ addGoalToList }) => {
+const InputGoals = ({ addGoalToList, isModalOpen, closeModal }) => {
     const [currentGoal, setcurrentGoal] = useState('');
 
     const currentGoalChangeHandler = (newGoal) => {
@@ -10,32 +10,46 @@ const InputGoals = ({ addGoalToList }) => {
 
     const styles = StyleSheet.create({
         inputContainer: {
-            marginVertical: 30,
-            flexDirection: 'row',
-            justifyContent: "space-evenly",
+            flex: 1,
+            justifyContent: "center",
             alignItems: 'center'
         },
-
         input: {
             borderBottomColor: "black",
             borderBottomWidth: 1,
+            width: "80%",
             padding: 10,
-            flex: 3,
-            marginRight: 10,
+            marginVertical: 30,
         },
-        inputButton: {
+        btngrp: {
+            width: "60%",
+            flexDirection: "row",
+            justifyContent: "space-between"
+        },
+        btn: {
+            marginHorizontal: 5,
             flex: 1,
-        }
+        },
     })
     return (
-        <View style={styles.inputContainer}>
-            {/* alignItems for cross axis */}
-            {/* justify content for main axis */}
-            <TextInput placeholder="Enter your Goal" style={styles.input}
-                onChangeText={currentGoalChangeHandler}
-                value={currentGoal}></TextInput>
-            <Button title="ADD" onPress={addGoalToList.bind(this, currentGoal)} style={styles.inputButton}></Button>
-        </View>
+        <Modal visible={isModalOpen} animationType="slide">
+            <View style={styles.inputContainer}>
+                {/* alignItems for cross axis */}
+                {/* justify content for main axis */}
+                <TextInput placeholder="Enter your Goal" style={styles.input}
+                    onChangeText={currentGoalChangeHandler}
+                    value={currentGoal}>
+                </TextInput>
+                <View style={styles.btngrp}>
+                    <View style={styles.btn}>
+                        <Button title="Add" onPress={() => { addGoalToList(currentGoal); closeModal(); }}></Button>
+                    </View>
+                    <View style={styles.btn}>
+                        <Button title="Cancel" color="red" onPress={() => { closeModal(); }}></Button>
+                    </View>
+                </View>
+            </View>
+        </Modal>
     );
 };
 
